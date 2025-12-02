f73e28f3eaa2a0f1ed84f0f1f3a2f253f32c0e0f
Rob Walker <robertsrwalker@gmail.com>
2025-12-02 06:24:31 -0800

    Add more keepsake rationales

--- src/routes/keepsakes/+page.svelte
+++ src/routes/keepsakes/+page.svelte
@@ -10,36 +10,54 @@
 			<ul class="list-disc list-outside ml-5 space-y-1">
 				<Keepsake
 					item="<i>The Left Hand of Darkness</i> by Ursula K. Le Guin"
-					rationale="My friend Nathan O recommended this book and it is one of my favorites. I read this book because I love s.f. but have always cringed at some of the <q>golden age</q> s.f. (think Isaac Asimov or Philip K. Dick) where space cowboys solve galactic problems with technology. In contrast, Le Guin uses s.f. in the Left Hand of Darkness as a lens through which to explore social issues (in this case gender) in ways that would be otherwise impossible within the constraints of our own universe. This book was beautiful, liberating, and way ahead of its time, continues to remind me of the lens through which I live in this world and what could be."
+					rationale="This book changed how I view sf and gender. I originally read this book on recommendation from my friend Nathan and because I love sf but have always cringed at some of the <q>golden age</q> sf (think Isaac Asimov or Philip K. Dick) where space cowboys slash great men of science solve galactic problems with technology. In contrast, Le Guin uses sf in <i>The Left Hand of Darkness</i> as a lens through which to explore social issues (gender) in ways that would be otherwise impossible within the constraints of our own universe. This book was beautiful, liberating, and way ahead of its time, and it helped me further develop my perspective on gender/binaries and the alternatives that could be."
 				/>
 				<Keepsake
 					item="<i>Blood Meridian</i> by Cormac McCarthy"
-					rationale="My favorite teacher Dr. Churchward gifted me this book as the high school junior year English prize (I peaked early). However, I didn't read it until two years later and was awed. It is one of the most violent and disturbing pieces of media I have ever consumed. It is also one of the best. I love that it deromanticizes the American westward expansion from a glorified Lewis and Clark expedition to what it was probably more like in reality — an incomprehensibly bloody, violent, and lawless expansion. The Judge remains my favorite villain of all time. He is terrifying and genuinely evil but also so weirdly likeable in a way that kind of reminds me of Satan from <i>Paradise Lost</i> or Logan Roy from <q>Succession</q>. Whenever I think about this book I picture the Judge dancing and his chilling immortality and usually unintentionally say <q>wow</q> out loud."
+					rationale="My favorite high school teacher Dr. Churchward gifted me this book as the junior year English prize (I peaked early), but I only read it two years later. I was awed. It is one of the most violent and disturbing pieces of media I have ever consumed. It is also one of the best. I love that it deromanticizes the American westward expansion from a glorified Lewis and Clark expedition to what it was probably more like in reality — an incomprehensibly bloody and lawless expansion. The Judge remains my favorite villain of all time. He is terrifying and genuinely evil but also so weirdly likeable in a way that kind of reminds me of Satan from <i>Paradise Lost</i> or Logan Roy from <q>Succession.</q> Whenever I think about this book I picture the Judge dancing and his chilling immortality and usually unintentionally say <q>wow</q> out loud to the surprise of others around me."
 				/>
 				<Keepsake
 					item="<i>East of Eden</i> by John Steinbeck"
-					rationale="I was sobbing for probably the last 150 pages of this book. As someone who struggles with shame and judgement and being <q>Good</q> (especially within the context of a father-son relationship), this book hit hard. Stenbeck was my first favorite author (so I associate him with developing my own sense of taste) and I often think of his affectionate writing on California whenever I venture into the valleys/hills/mountains around SF."
+					rationale="I was sobbing for probably the last 150 pages of this book. As someone who struggles with shame and judgement and being <q>Good</q> (especially within the context of a father-son relationship), this book hit hard. Stenbeck was my first favorite author (so I associate him with developing my own sense of taste) and I often think of his affectionate writing on California whenever I venture into the valleys/hills/mountains around SF. My favorite quote from this book: <q>And now that you don't have to be perfect, you can be good.</q>"
 				/>
 			</ul>
 		</div>
 		<div>
 			<h3 class="font-sans text-lg">Poetry</h3>
 			<ul class="list-disc list-outside ml-5 space-y-1">
-				<Keepsake item='<q>Masks</q> by Shel Silverstein' rationale="TODO" />
-				<Keepsake item='<q>The Missing Piece</q> by Shel Silverstein' rationale="TODO" />
-				<Keepsake item='<q>One Art</q> by Elizabeth Bishop' rationale="TODO" />
-				<Keepsake item='<q>The Fish</q> by Elizabeth Bishop' rationale="TODO" />
-				<Keepsake item='<q>The Two-Headed Calf</q> by Laura Gilpin' rationale="TODO" />
-				<Keepsake item='<q>Sick Goat</q> by Mikko Harvey' rationale="TODO" />
+				<Keepsake
+					item='<q>Masks</q> by Shel Silverstein'
+					rationale="I have pretty strong social anxiety and so struggle with <q>being myself</q> + have a tendency for hypervigilance and performance around others. This brief poem is a consistent reminder for me of the costs of performance and inauthenticity. Also, one of the last pieces of advice my grandmother gave before passing at 103yrs old was to be yourself bc life is too short to be anything else. Lastly, I love that this is a poem for children and yet holds such a powerful message for adults." 
+				/>
+				<Keepsake
+					item='<q>The Missing Piece</q> by Shel Silverstein'
+					rationale="I generally struggle to feel like I fit in despite wanting to. When I feel more therapy-y I sometimes think this started with being the youngest of four boys with a decent age gap, meaning I was always seeking the validation and inclusion of people older/better than me. This was reinforced in high school where I was the only crew athlete in my year and so my main friend group was the year above. Sometimes, I even worry that as soon as I begin fitting in somewhere I panic because it feels stifling and unfamiliar — a toxic reaction that I try guarding against. Anyways, this poem reminds me that I don't have to fit in anywhere to be complete and can roll on my own while still having strong relationships with others."
+				/>
+				<Keepsake
+					item='<q>One Art</q> by Elizabeth Bishop'
+					rationale="Whenever I feel heartbroken or experience loss I read this poem and feel comforted knowing it will pass. I first read it during my junior year English class and it sparked my love of Bishop. My only qualm is the <q>(write it!)</q> in the last line, but it has grown on me over time."
+				/>
+				<Keepsake
+					item='<q>The Fish</q> by Elizabeth Bishop'
+					rationale="TODO"
+				/>
+				<Keepsake
+					item='<q>The Two-Headed Calf</q> by Laura Gilpin'
+					rationale="TODO"
+				/>
+				<Keepsake
+					item='<q>Sick Goat</q> by Mikko Harvey'
+					rationale="TODO"
+				/>
 			</ul>
 		</div>
 		<div>
 			<h3 class="font-sans text-lg">Videos</h3>
 			<ul class="list-disc list-outside ml-5 space-y-1">
-				<Keepsake item='<q>MAMA&#39;S EYES</q> by METTE (<a class="font-bold" href="https://www.youtube.com/watch?v=gi9OBRzbMNI">ref</a>)' rationale="TODO" />
-				<Keepsake item='<q>End of the Tour: The Problem of Loneliness</q> (<a class="font-bold" href="https://www.youtube.com/watch?v=FCfpOugmd9E">ref</a>)' rationale="TODO" />
-				<Keepsake item='<q>Spike Jonze for Apple HomePod — Welcome Home</q> (<a class="font-bold" href="https://www.youtube.com/watch?v=Xwc22EK8qE0">ref</a>)' rationale="TODO" />
-				<Keepsake item='<q>Fred Again..:Tiny Desk Concert</q> by Fred Again, NPR (<a class="font-bold" href="https://www.youtube.com/watch?v=4iQmPv_dTI0&t=178s">ref</a>)' rationale="TODO" />
+				<Keepsake item='<q>MAMA&#39;S EYES</q> by METTE (<a class="font-bold" href="https://www.youtube.com/watch?v=gi9OBRzbMNI">link</a>)' rationale="TODO" />
+				<Keepsake item='<q>End of the Tour: The Problem of Loneliness</q> (<a class="font-bold" href="https://www.youtube.com/watch?v=FCfpOugmd9E">link</a>)' rationale="TODO" />
+				<Keepsake item='<q>Spike Jonze for Apple HomePod — Welcome Home</q> (<a class="font-bold" href="https://www.youtube.com/watch?v=Xwc22EK8qE0">link</a>)' rationale="TODO" />
+				<Keepsake item='<q>Fred Again..:Tiny Desk Concert</q> by Fred Again, NPR (<a class="font-bold" href="https://www.youtube.com/watch?v=4iQmPv_dTI0&t=178s">link</a>)' rationale="TODO" />
 			</ul>
 		</div>
 		<div>
@@ -63,23 +81,17 @@
 			<h3 class="font-sans text-lg">Music</h3>
 			<ul class="list-disc list-outside ml-5 space-y-1">
 				<Keepsake item='<q>Think Fast</q> by Dominic Fike feat. Weezer' rationale="TODO" />
-				<Keepsake item='<q>LIE DOWN</q> by Saya Gray' rationale="TODO" />
 				<Keepsake item='<q>It&#39;s Called: Freefall</q> by Rainbow Kitten Surprise' rationale="TODO" />
-				<Keepsake item='<q>Painkillers</q> by Rainbow Kitten Surprise' rationale="TODO" />
+				<Keepsake item='<q>Flimsier</q> by King Krule' rationale="TODO" />
+				<Keepsake item='<q>LIE DOWN</q> by Saya Gray' rationale="TODO" />
+				<Keepsake item='<q>Leonard Cohen</q> by boygenius' rationale="TODO" />
+				<Keepsake item='<q>Hallelujah</q> by Jeff Buckley' rationale="TODO" />
+				<Keepsake item='<q>warm glow</q> by Hippo Campus' rationale="TODO" />
 				<Keepsake item='<q>Ballade No. 1 in G Minor Op. 23</q> by Chopin' rationale="TODO" />
 				<Keepsake item='<q>Ballade No. 4 in F Minor Op. 52</q> by Chopin' rationale="TODO" />
 				<Keepsake item='<q>Prelude in C-Sharp Minor, Op. 3, No. 2</q> by Rachmaninoff' rationale="TODO" />
-				<Keepsake item='<q>24 Caprices, Op. 1 No. 24 in A Minor</q> by Paganini' rationale="TODO" />
-				<Keepsake item='<q>La Campanella</q> by Liszt' rationale="TODO" />
-				<Keepsake item='<q>Nocturne Op. 55 no. 1</q> by Chopin' rationale="TODO" />
-				<Keepsake item='<q>Out Getting Ribs</q> by King Krule' rationale="TODO" />
-				<Keepsake item='<q>Ocean Bed</q> by King Krule' rationale="TODO" />
-				<Keepsake item='<q>Leonard Cohen</q> by boygenius' rationale="TODO" />
-				<Keepsake item='<q>Hallelujah</q> by Jeff Buckley' rationale="TODO" />
-				<Keepsake item='<q>Disappearing</q> by Low' rationale="TODO" />
 				<Keepsake item='<q>Heart-Shaped Box</q> by Nirvana' rationale="TODO" />
-				<Keepsake item='<q>warm glow</q> by Hippo Campus' rationale="TODO" />
-				<Keepsake item='<q>Mythological Beauty</q> by Big Thief' rationale="TODO" />
+				<Keepsake item='<q>Seigfried</q> by Frank Ocean' rationale="TODO" />
 			</ul>
 		</div>
 		<div>
@@ -95,12 +107,12 @@
 			<ul class="list-disc list-outside ml-5 space-y-1">
 				<Keepsake item='<q>Live by the harmless untruths that make you brave and kind and healthy and happy.</q> from <i>Cat&#39;s Cradle</i> by Kurt Vonnegut' rationale="TODO" />
 				<Keepsake item='<q>Why would you believe in something awful when you could believe in something wonderful?</q> from <i>Fleabag</i> by Phoebe Waller-Bridge' rationale="TODO" />
-				<Keepsake item='<q>When I am served half an egg at a restaurant, I wonder to myself: who has the other half of my egg? Two strangers; living their lives; sharing an egg.</q> (<a class="font-bold" href="https://x.com/Junferno/status/1841070931336445993">ref</a>)' rationale="TODO" />
-				<Keepsake item='<q>pretty shitty how baseline human activities like singing, dancing and making art got turned into skills instead of being seen as behaviors \ so now it&#39;s like the point of doing them is to get good at them&#39; and not &#39;this is a thing humans do, the way birds sing and bees make hives&#39;.</q> (<a class="font-bold" href="https://x.com/rubycumulous/status/1388581085605474306">ref</a>)' rationale="TODO" />
+				<Keepsake item='<q>In romantic relationships, you should always be yourself, because the worst that can happen is something that wasn&#39;t going to work out doesn&#39;t.</q> from a 2018 lecture by Louis Menand in Humanities 10a (paraphrased)' rationale="TODO" />
 				<Keepsake item='<q>We rip out so much of ourselves to be cured of things faster that we go bankrupt by the age of thirty, and have less to offer each time we start with someone new. But to make yourself feel nothing, so as not to feel anything — what a waste!</q> from <i>Call Me By Your Name</i> by André Aciman' rationale="TODO" />
-				<Keepsake item='<q>We live in capitalism. Its power seems inescapable. So did the divine right of kings. Any human power can be resisted and changed by human beings. Resistance and change often begin in art, and very often in our art, the art of words.</q> by Ursula K. Le Guin (<a class="font-bold" href="https://www.youtube.com/watch?v=s2v7RDyo7os&t=610s">ref</a>)' rationale="TODO" />
+				<Keepsake item='<q>We live in capitalism. Its power seems inescapable. So did the divine right of kings. Any human power can be resisted and changed by human beings. Resistance and change often begin in art, and very often in our art, the art of words.</q> by Ursula K. Le Guin (<a class="font-bold" href="https://www.youtube.com/watch?v=s2v7RDyo7os&t=610s">link</a>)' rationale="TODO" />
 				<Keepsake item='<q>For what is grief, but love persevering?</q> from WandaVision' rationale="TODO" />
-				<Keepsake item='<q>In romantic relationships, you should always be yourself, because the worst thing that can happen is something that wasn&#39;t going to work out doesn&#39;t.</q> from a 2018 lecture by Louis Menand in Humanities 10a (paraphrased)' rationale="TODO" />
+				<Keepsake item='<q>When I am served half an egg at a restaurant, I wonder to myself: who has the other half of my egg? Two strangers; living their lives; sharing an egg.</q> (<a class="font-bold" href="https://x.com/Junferno/status/1841070931336445993">link</a>)' rationale="TODO" />
+				<Keepsake item='<q>pretty shitty how baseline human activities like singing, dancing and making art got turned into skills instead of being seen as behaviors \ so now it&#39;s like the point of doing them is to get good at them&#39; and not &#39;this is a thing humans do, the way birds sing and bees make hives&#39;.</q> (<a class="font-bold" href="https://x.com/rubycumulous/status/1388581085605474306">link</a>)' rationale="TODO" />
 			</ul>
 		</div>
 	</div>
