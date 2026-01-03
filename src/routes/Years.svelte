1a3e610613741db55c1e65dcde2a25f96bfb1948
Rob Walker <robertsrwalker@gmail.com>
2026-01-02 21:14:02 -0500

    Add more years

--- src/routes/Years.svelte
+++ src/routes/Years.svelte
@@ -5,6 +5,171 @@
 </script>
 
 <div class="flex flex-col space-y-3 mt-8">
+{#if selectedRingId === 1}
+    <Month month="April">
+        Turn 1 years old, I am a blob.
+    </Month>
+{/if}
+{#if selectedRingId === 2}
+    <Month month="April">
+        Turn 2 years old, likely start babbling a couple words.
+    </Month>
+{/if}
+{#if selectedRingId === 3}
+    <Month month="April">
+        Turn 3 years old, start anunciating.
+    </Month>
+{/if}
+{#if selectedRingId === 4}
+    <Month month="April">
+        Turn 4 years old.
+    </Month>
+{/if}
+{#if selectedRingId === 5}
+    <Month month="April">
+        Turn 5 years old.
+    </Month>
+{/if}
+{#if selectedRingId === 6}
+    <Month month="September">
+        Start Grade 1 at the York School.
+    </Month>
+{/if}
+{#if selectedRingId === 7}
+    <Month month="September">
+        Start Grade 2 at the York School.
+    </Month>
+{/if}
+{#if selectedRingId === 8}
+    <Month month="September">
+        Start Grade 3 at the York School.
+    </Month>
+{/if}
+{#if selectedRingId === 9}
+    <Month month="September">
+        Start Grade 4 at the York School.
+    </Month>
+{/if}
+{#if selectedRingId === 10}
+    <Month month="September">
+        Start Grade 5 at UCC.
+    </Month>
+{/if}
+{#if selectedRingId === 11}
+    <Month month="September">
+        Start Grade 6 at UCC.
+    </Month>
+{/if}
+{#if selectedRingId === 12}
+    <Month month="January">
+        Concussion from skiing.
+    </Month>
+    <Month month="September">
+        Start Grade 7 at UCC.
+    </Month>
+{/if}
+{#if selectedRingId === 13}
+    <Month month="September">
+        Start Grade 8 at UCC.
+    </Month>
+{/if}
+{#if selectedRingId === 14}
+    <Month month="September">
+        Start Grade 9 at UCC.
+    </Month>
+    <Month month="November">
+        Join the crew team.
+    </Month>
+{/if}
+{#if selectedRingId === 15}
+    <Month month="September">
+        Start Grade 10 at UCC.
+    </Month>
+{/if}
+{#if selectedRingId === 16}
+    <Month month="September">
+        Start Grade 11 at UCC.
+    </Month>
+{/if}
+{#if selectedRingId === 17}
+    <Month month="September">
+        Start Grade 12 at UCC.
+    </Month>
+{/if}
+{#if selectedRingId === 18}
+    <Month month="May">
+        Decide to take a Gap Year before college.
+    </Month>
+    <Month month="September">
+        Start working at startup.
+    </Month>
+{/if}
+{#if selectedRingId === 19}
+    <Month month="February">
+        Stop working at startup, start travelling in Southeast Asia.
+    </Month>
+    <Month month="May">
+        Stop travelling in Southeast Asia, take Canadian Securities Course.
+    </Month>
+    <Month month="September">
+        Start first year at Harvard.
+    </Month>
+{/if}
+{#if selectedRingId === 20}
+    <Month month="September">
+        Start sophomore year at Harvard.
+    </Month>
+{/if}
+{#if selectedRingId === 21}
+    <Month month="March">
+        COVID pandemic hits, move home during spring break.
+    </Month>
+    <Month month="May">
+        Decide to take gap year during COVID.
+    </Month>
+    <Month month="September">
+        Start working at Shopify.
+    </Month>
+{/if}
+{#if selectedRingId === 22}
+    <Month month="June">
+        Stop working at Shopify.
+    </Month>
+    <Month month="July">
+        Travel to Michigan and Boston and Montana.
+    </Month>
+    <Month month="August">
+        Lead digital FOP.
+    </Month>
+    <Month month="September">
+        Start junior year at Harvard.
+    </Month>
+{/if}
+{#if selectedRingId === 23}
+    <Month month="September">
+        Start senior year at Harvard.
+    </Month>
+{/if}
+{#if selectedRingId === 24}
+    <Month month="May">
+        Graduate from Harvard.
+    </Month>
+    <Month month="June">
+        Hike the Sawtooths, hike the Skyline Trail, hike the Rockwall Trail.
+    </Month>
+    <Month month="June">
+        Visit Prague and Munich, hike the TMB, visit Berlin.
+    </Month>
+    <Month month="August">
+        Hike an ECT section (solo), hike the Collegiate Peaks West, canoe trip in Boundary Waters, support FOP base camp.
+    </Month>
+    <Month month="September">
+        Move to SF, romantic weekend.
+    </Month>
+    <Month month="October">
+        Start working at Meta.
+    </Month>
+{/if}
 {#if selectedRingId === 25}
     <Month month="January">
         Tahoe skip trip, start growing Indigo and Lavendar and Tobacco from seed.
